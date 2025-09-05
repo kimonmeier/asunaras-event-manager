e18219c3e1449962a7f56338cf48927b48b5159c
Kimon Meier <kimon.meier@gmail.com>
2025-09-05 23:45:21 +0200

    Add logging and optimize feedback handling in event completion
    This commit introduces logging to the `EventCompletedEventHandler` for improved traceability. The feedback handling was also adjusted for better performance using `ConfigureAwait(false)` and logging was added to record completed events.

--- Asunara-Event-Manager/Events/EventCompleted/EventCompletedEventHandler.cs
+++ Asunara-Event-Manager/Events/EventCompleted/EventCompletedEventHandler.cs
@@ -2,6 +2,7 @@
 using EventManager.Data.Repositories;
 using EventManager.Events.EventStartFeedback;
 using MediatR;
+using Microsoft.Extensions.Logging;
 
 namespace EventManager.Events.EventCompleted;
 
@@ -10,12 +11,14 @@ public class EventCompletedEventHandler : IRequestHandler<EventCompletedEvent>
     private readonly DiscordEventRepository _discordEventRepository;
     private readonly DbTransactionFactory _dbTransactionFactory;
     private readonly ISender _sender;
+    private readonly ILogger<EventCompletedEventHandler> _logger;
 
-    public EventCompletedEventHandler(DbTransactionFactory dbTransactionFactory, DiscordEventRepository discordEventRepository, ISender sender)
+    public EventCompletedEventHandler(DbTransactionFactory dbTransactionFactory, DiscordEventRepository discordEventRepository, ISender sender, ILogger<EventCompletedEventHandler> logger)
     {
         _dbTransactionFactory = dbTransactionFactory;
         _discordEventRepository = discordEventRepository;
         _sender = sender;
+        _logger = logger;
     }
 
     public async Task Handle(EventCompletedEvent request, CancellationToken cancellationToken)
@@ -26,15 +29,19 @@ public class EventCompletedEventHandler : IRequestHandler<EventCompletedEvent>
             throw new Exception("Event not found");
         }
 
-        await _sender.Send(new EventStartFeedbackEvent()
-        {
-            Event = @event,
-        }, cancellationToken);
-        
         Transaction transaction = _dbTransactionFactory.CreateTransaction();
 
         @event.IsCompleted = true;
         await _discordEventRepository.UpdateAsync(@event);
         await transaction.Commit(cancellationToken);
+
+        Task eventStartFeedback = _sender.Send(new EventStartFeedbackEvent()
+        {
+            Event = @event,
+        }, cancellationToken);
+
+        eventStartFeedback.ConfigureAwait(false).GetAwaiter();
+        
+        _logger.LogInformation("Event completed: {0}", @event.Name);;
     }
 }
