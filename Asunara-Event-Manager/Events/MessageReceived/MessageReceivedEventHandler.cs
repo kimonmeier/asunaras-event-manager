348b258dc40cf47d03a5b44bbff44d0cabea6843
Kimon Meier <kimon.meier@gmail.com>
2025-09-28 02:42:08 +0200

    Add channel exclusion logic to MessageReceived handler
    Introduced a check to skip processing activity events for channels listed in the excluded channels configuration. Added logging to indicate when activity is disabled for a channel, ensuring better traceability and configurability.

--- Asunara-Event-Manager/Events/MessageReceived/MessageReceivedEventHandler.cs
+++ Asunara-Event-Manager/Events/MessageReceived/MessageReceivedEventHandler.cs
@@ -1,7 +1,9 @@
-﻿using EventManager.Data;
+﻿using EventManager.Configuration;
+using EventManager.Data;
 using EventManager.Data.Entities.Activity;
 using EventManager.Data.Repositories;
 using MediatR;
+using Microsoft.Extensions.Logging;
 
 namespace EventManager.Events.MessageReceived;
 
@@ -9,17 +11,27 @@ public class MessageReceivedEventHandler : IRequestHandler<MessageReceivedEvent>
 {
     private readonly DbTransactionFactory _dbTransactionFactory;
     private readonly ActivityEventRepository _activityEventRepository;
+    private readonly RootConfig _rootConfig;
+    private readonly ILogger<MessageReceivedEventHandler> _logger;
 
-    public MessageReceivedEventHandler(DbTransactionFactory dbTransactionFactory, ActivityEventRepository activityEventRepository)
+    public MessageReceivedEventHandler(DbTransactionFactory dbTransactionFactory, ActivityEventRepository activityEventRepository, RootConfig rootConfig, ILogger<MessageReceivedEventHandler> logger)
     {
         _dbTransactionFactory = dbTransactionFactory;
         _activityEventRepository = activityEventRepository;
+        _rootConfig = rootConfig;
+        _logger = logger;
     }
 
     public async Task Handle(MessageReceivedEvent request, CancellationToken cancellationToken)
     {
+        if (_rootConfig.Discord.Activity.ExcludedChannelsId.Contains(request.Message.Channel.Id))
+        {
+            _logger.LogDebug($"Die Activity in dem Channel {request.Message.Channel.Id} ist deaktiviert!");
+            return;
+        }
+        
         var transaction = await _dbTransactionFactory.CreateTransaction();
-
+        
         await _activityEventRepository.AddAsync(new ActivityEvent()
         {
             Date = DateTime.UtcNow, ChannelId = request.Message.Channel.Id, DiscordUserId = request.Message.Author.Id,
