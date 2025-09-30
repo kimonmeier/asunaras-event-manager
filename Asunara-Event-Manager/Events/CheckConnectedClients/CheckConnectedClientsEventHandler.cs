8619afaa79e709249c2f9e16e5c19f6333047b6b
Kimon Meier <kimon.meier@gmail.com>
2025-09-30 02:01:49 +0200

    Simplify conditional check for voice activity type in `CheckConnectedClients` handler.

--- Asunara-Event-Manager/Events/CheckConnectedClients/CheckConnectedClientsEventHandler.cs
+++ Asunara-Event-Manager/Events/CheckConnectedClients/CheckConnectedClientsEventHandler.cs
@@ -40,12 +40,7 @@ public class CheckConnectedClientsEventHandler : IRequestHandler<CheckConnectedC
             
             var activity = await _activityEventRepository.GetLastVoiceActivityByDiscordId(connectedUser.Id);
 
-            if (activity is null)
-            {
-                continue;
-            }
-
-            if (activity.Type == ActivityType.VoiceChannelLeft)
+            if (activity is not null && activity.Type == ActivityType.VoiceChannelLeft)
             {
                 continue;
             }
