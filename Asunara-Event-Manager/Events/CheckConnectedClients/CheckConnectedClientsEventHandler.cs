c9d1f004f3f5130c3afa7114d98871366133df78
Kimon Meier <kimon.meier@gmail.com>
2025-10-03 15:10:17 +0200

    Refactor `CheckConnectedClientsEventHandler` to improve readability and optimize collection usage through `HashSet` integration

--- Asunara-Event-Manager/Events/CheckConnectedClients/CheckConnectedClientsEventHandler.cs
+++ Asunara-Event-Manager/Events/CheckConnectedClients/CheckConnectedClientsEventHandler.cs
@@ -21,58 +21,58 @@ public class CheckConnectedClientsEventHandler : IRequestHandler<CheckConnectedC
         _sender = sender;
     }
 
+
     public async Task Handle(CheckConnectedClientsEvent request, CancellationToken cancellationToken)
     {
-        List<ulong> channelIds = new List<ulong>();
-        List<ulong> wronglyConnectedUserIds = await _activityEventRepository.GetUserIdsCurrentlyConnectedToVoiceChannels();
+        var channelIds = new HashSet<ulong>();
+        var wronglyConnectedUserIds = (await _activityEventRepository.GetUserIdsCurrentlyConnectedToVoiceChannels()).ToHashSet();
+
         foreach (SocketGuildUser connectedUser in request.ConnectedUsers)
         {
-            if (!connectedUser.VoiceState.HasValue)
-            {
-                continue;
-            }
-
-            var voiceState = connectedUser.VoiceState.Value;
-            
-            if (!channelIds.Contains(voiceState.VoiceChannel.Id))
-            {
-                channelIds.Add(voiceState.VoiceChannel.Id);
-            }
-            
-            if (wronglyConnectedUserIds.Contains(connectedUser.Id))
-            {
-                wronglyConnectedUserIds.Remove(connectedUser.Id);
-            }
-            
-            var activity = await _activityEventRepository.GetLastVoiceActivityByDiscordId(connectedUser.Id);
-
-            if (activity is null)
-            {
-                continue;           
-            }
-            
-            if (activity.Type != ActivityType.VoiceChannelLeft && activity.ChannelId == voiceState.VoiceChannel.Id)
-            {
-                continue;
-            }
-            
-            Transaction transaction = await _dbTransactionFactory.CreateTransaction();
-
-            await _activityEventRepository.AddAsync(new ActivityEvent()
-            {
-                Date = DateTime.UtcNow, Type = ActivityType.VoiceChannelJoined, ChannelId = voiceState.VoiceChannel.Id, DiscordUserId = connectedUser.Id
-            });
-
-            await transaction.Commit(cancellationToken);
+            await ProcessConnectedUser(connectedUser, channelIds, wronglyConnectedUserIds, cancellationToken);
         }
 
         await CheckChannelsForActivity(channelIds, cancellationToken);
-        
         await MarkDisconnectedClients(wronglyConnectedUserIds, cancellationToken);
     }
-    
-    private async Task MarkDisconnectedClients(IList<ulong> users, CancellationToken cancellationToken = default)
-    {        
+
+    private async Task ProcessConnectedUser(SocketGuildUser connectedUser, HashSet<ulong> channelIds, HashSet<ulong> wronglyConnectedUserIds, CancellationToken cancellationToken)
+    {
+        var activity = await _activityEventRepository.GetLastVoiceActivityByDiscordId(connectedUser.Id);
+        var voiceState = connectedUser.VoiceState!.Value;
+
+        channelIds.Add(voiceState.VoiceChannel.Id);
+        wronglyConnectedUserIds.Remove(connectedUser.Id);
+
+        if (activity is null)
+        {
+            await CreateEntity(connectedUser.Id, voiceState.VoiceChannel.Id, cancellationToken);
+
+            return;
+        }
+
+        if (activity.Type != ActivityType.VoiceChannelLeft && activity.ChannelId == voiceState.VoiceChannel.Id)
+        {
+            return;
+        }
+
+        await CreateEntity(connectedUser.Id, voiceState.VoiceChannel.Id, cancellationToken);
+    }
+
+    private async Task CreateEntity(ulong discordUserId, ulong channelId, CancellationToken cancellationToken)
+    {
+        Transaction transaction = await _dbTransactionFactory.CreateTransaction();
+
+        await _activityEventRepository.AddAsync(new ActivityEvent()
+        {
+            Date = DateTime.UtcNow, Type = ActivityType.VoiceChannelJoined, ChannelId = channelId, DiscordUserId = discordUserId
+        });
+
+        await transaction.Commit(cancellationToken);
+    }
+
+    private async Task MarkDisconnectedClients(HashSet<ulong> users, CancellationToken cancellationToken = default)
+    {
         Transaction transactionConnected = await _dbTransactionFactory.CreateTransaction();
         foreach (var connectedClient in users)
         {
@@ -82,11 +82,11 @@ public class CheckConnectedClientsEventHandler : IRequestHandler<CheckConnectedC
                 Date = DateTime.UtcNow, Type = ActivityType.VoiceChannelLeft, ChannelId = lastVoiceActivityByDiscordId.ChannelId, DiscordUserId = connectedClient
             });
         }
-        
+
         await transactionConnected.Commit(cancellationToken);
     }
 
-    private async Task CheckChannelsForActivity(IList<ulong> channelIds, CancellationToken cancellationToken = default)
+    private async Task CheckChannelsForActivity(HashSet<ulong> channelIds, CancellationToken cancellationToken = default)
     {
         foreach (ulong channelId in channelIds)
         {
